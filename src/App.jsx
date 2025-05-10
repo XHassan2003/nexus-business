147782ed8eeefb8807affb77b7bbda14a6cc8230
XHassan2003 <iamzoom567@gmail.com>
2025-05-10 17:46:20 +0500

    Description and fevicon for the app

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-// import StartupDashboard from "./pages/StartupDashboard";
-// import InvestorDashboard from "./pages/InvestorDashboard";
 import Dashboard from './pages/Dashboard';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
